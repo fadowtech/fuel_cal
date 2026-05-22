95d78e338509412cc15b6ee242ec04cdc444c043
Fadow Tech <fadowtech.com>
2026-05-22 18:30:49 +0530

    Updated project files

---
+++ lib/models/expense_model.freezed.dart
@@ -0,0 +1,293 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'expense_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Expense _$ExpenseFromJson(Map<String, dynamic> json) {
+  return _Expense.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Expense {
+  int get id => throw _privateConstructorUsedError;
+  @JsonKey(name: 'user_id')
+  int get userId => throw _privateConstructorUsedError;
+  @JsonKey(name: 'vehicle_id')
+  int? get vehicleId => throw _privateConstructorUsedError;
+  String get category => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  double get amount => throw _privateConstructorUsedError;
+  DateTime? get date => throw _privateConstructorUsedError;
+
+  /// Serializes this Expense to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Expense
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ExpenseCopyWith<Expense> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ExpenseCopyWith<$Res> {
+  factory $ExpenseCopyWith(Expense value, $Res Function(Expense) then) =
+      _$ExpenseCopyWithImpl<$Res, Expense>;
+  @useResult
+  $Res call(
+      {int id,
+      @JsonKey(name: 'user_id') int userId,
+      @JsonKey(name: 'vehicle_id') int? vehicleId,
+      String category,
+      String title,
+      double amount,
+      DateTime? date});
+}
+
+/// @nodoc
+class _$ExpenseCopyWithImpl<$Res, $Val extends Expense>
+    implements $ExpenseCopyWith<$Res> {
+  _$ExpenseCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Expense
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? userId = null,
+    Object? vehicleId = freezed,
+    Object? category = null,
+    Object? title = null,
+    Object? amount = null,
+    Object? date = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      userId: null == userId
+          ? _value.userId
+          : userId // ignore: cast_nullable_to_non_nullable
+              as int,
+      vehicleId: freezed == vehicleId
+          ? _value.vehicleId
+          : vehicleId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      amount: null == amount
+          ? _value.amount
+          : amount // ignore: cast_nullable_to_non_nullable
+              as double,
+      date: freezed == date
+          ? _value.date
+          : date // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ExpenseImplCopyWith<$Res> implements $ExpenseCopyWith<$Res> {
+  factory _$$ExpenseImplCopyWith(
+          _$ExpenseImpl value, $Res Function(_$ExpenseImpl) then) =
+      __$$ExpenseImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      @JsonKey(name: 'user_id') int userId,
+      @JsonKey(name: 'vehicle_id') int? vehicleId,
+      String category,
+      String title,
+      double amount,
+      DateTime? date});
+}
+
+/// @nodoc
+class __$$ExpenseImplCopyWithImpl<$Res>
+    extends _$ExpenseCopyWithImpl<$Res, _$ExpenseImpl>
+    implements _$$ExpenseImplCopyWith<$Res> {
+  __$$ExpenseImplCopyWithImpl(
+      _$ExpenseImpl _value, $Res Function(_$ExpenseImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Expense
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? userId = null,
+    Object? vehicleId = freezed,
+    Object? category = null,
+    Object? title = null,
+    Object? amount = null,
+    Object? date = freezed,
+  }) {
+    return _then(_$ExpenseImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      userId: null == userId
+          ? _value.userId
+          : userId // ignore: cast_nullable_to_non_nullable
+              as int,
+      vehicleId: freezed == vehicleId
+          ? _value.vehicleId
+          : vehicleId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      amount: null == amount
+          ? _value.amount
+          : amount // ignore: cast_nullable_to_non_nullable
+              as double,
+      date: freezed == date
+          ? _value.date
+          : date // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ExpenseImpl implements _Expense {
+  const _$ExpenseImpl(
+      {required this.id,
+      @JsonKey(name: 'user_id') required this.userId,
+      @JsonKey(name: 'vehicle_id') this.vehicleId,
+      required this.category,
+      required this.title,
+      required this.amount,
+      this.date});
+
+  factory _$ExpenseImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ExpenseImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  @JsonKey(name: 'user_id')
+  final int userId;
+  @override
+  @JsonKey(name: 'vehicle_id')
+  final int? vehicleId;
+  @override
+  final String category;
+  @override
+  final String title;
+  @override
+  final double amount;
+  @override
+  final DateTime? date;
+
+  @override
+  String toString() {
+    return 'Expense(id: $id, userId: $userId, vehicleId: $vehicleId, category: $category, title: $title, amount: $amount, date: $date)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ExpenseImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.userId, userId) || other.userId == userId) &&
+            (identical(other.vehicleId, vehicleId) ||
+                other.vehicleId == vehicleId) &&
+            (identical(other.category, category) ||
+                other.category == category) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.amount, amount) || other.amount == amount) &&
+            (identical(other.date, date) || other.date == date));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, id, userId, vehicleId, category, title, amount, date);
+
+  /// Create a copy of Expense
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ExpenseImplCopyWith<_$ExpenseImpl> get copyWith =>
+      __$$ExpenseImplCopyWithImpl<_$ExpenseImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ExpenseImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Expense implements Expense {
+  const factory _Expense(
+      {required final int id,
+      @JsonKey(name: 'user_id') required final int userId,
+      @JsonKey(name: 'vehicle_id') final int? vehicleId,
+      required final String category,
+      required final String title,
+      required final double amount,
+      final DateTime? date}) = _$ExpenseImpl;
+
+  factory _Expense.fromJson(Map<String, dynamic> json) = _$ExpenseImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  @JsonKey(name: 'user_id')
+  int get userId;
+  @override
+  @JsonKey(name: 'vehicle_id')
+  int? get vehicleId;
+  @override
+  String get category;
+  @override
+  String get title;
+  @override
+  double get amount;
+  @override
+  DateTime? get date;
+
+  /// Create a copy of Expense
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ExpenseImplCopyWith<_$ExpenseImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
